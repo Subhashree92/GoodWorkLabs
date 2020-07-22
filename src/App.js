4958cebcd5b5d8616ef62983c16d24422f26c813
Subhashree1992 <sacharya@chatsoft.com>
2020-07-22 11:04:25 +0530

    code setup and write flxi component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Flexi from "./flexi";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const flexiConfig = {
+  items: [
+      {
+        "name": "person_name",
+        "label": "Person's Name",
+        "type": 'TextField',
+      },
+      {
+        "name": "states",
+        "label": "Person's state",
+        "type": "DropDown",
+            "values": [
+            "Maharashtra",
+            "Kerala",
+            "Tamil Nadu"
+            ]
+      }
+    ]
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "SomeOne",
+      place: 'Somewhere',      
+    };
+  }
+
+  onFlexSubmit = (val) => {
+    this.setState({ name: val.name, place: val.dd });
+  }
+
+  render() {
+    return (
+      <div>
+        <Flexi onSubmitFn={this.onFlexSubmit} config={flexiConfig.items} />
+    <p><i>{this.state.name}</i> From <i>{this.state.place}</i></p>
+      </div>
+    );
+  }
 }
 
 export default App;
